b582c630c0346904c50d13a6df7bce439bc6b88a
NitishBharadwajB26 <nitishbharadwajb@gmail.com>
2021-06-24 16:18:10 +0530

    Error handling

---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpClient
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private route: Router, private toastr: ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError(error => {
+        if(error)
+        {
+          switch(error.status){
+            case 400:
+            if(error.error.errors)
+            {
+              //StateModalErrors is called as Validation type of errors in ASP.NET Errors
+              const StateModalError = [];
+              for(const key in error.error.errors)
+              {
+                if(error.error.errors[key]){
+                  StateModalError.push(error.error.errors[key]);
+                }
+              }
+              throw StateModalError.flat();
+            } else {
+              this.toastr.error(error.statusText, error.status);
+            }
+            break;
+            case 401:
+              this.toastr.error(error.statusText, error.status);
+              break;
+            case 404:
+              this.route.navigateByUrl('/not-found');
+              break;
+            case 500:
+              const navigateByExtras: NavigationExtras =  {state:{error:error.error}};
+              this.route.navigateByUrl('/server-error', navigateByExtras);
+              break;
+            default :
+              this.toastr.error('Something unexpected went wrong');
+              console.log(error);
+              break;
+
+        }
+      }
+      return throwError(error);
+      })
+    )
+  }
+}
